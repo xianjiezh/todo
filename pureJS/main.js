781eca06f6a90f5ab92ac45e015bb419b15965cc
xianjiezh <904292766@qq.com>
2017-12-14 19:51:16 +0800

    add 'add' button

--- pureJS/main.js
+++ pureJS/main.js
@@ -2,22 +2,24 @@
 
 
 
-let addButton = document.querySelector('id-input-todo')
+let addButton = document.querySelector('#id-button-add')
 addButton.addEventListener('click', function () {
     let todoInput = document.querySelector('#id-input-todo')
-    let todo = todoInput.nodeValue
-    let todoContainer = document.querySelector('id-div-container')
+    let todo = todoInput.value
+    console.log(todo)
+    let todoContainer = document.querySelector('#id-div-container')
     let t = templateTodo(todo)
-    todoContainer.insertAdjacentElement('beforeend',t)
+    todoContainer.innerHTML = t
 }, false)
 
 function templateTodo(todo){
-    var t = `<div class="todo-cell">
-                <button class="todo-down">完成</button>
-                <button class="todo delete">删除</button>
-                <span contenteditable="ture">${todo}</span>
-            </div>
-        `
+    let t = `
+    <div class="todo-cell">
+        <button class="todo-down">完成</button>
+        <button class="todo delete">删除</button>
+        <span contenteditable="ture">${todo}</span>
+    </div>
+`
     return t
 }
     
