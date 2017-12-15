762f32927051e3c65459ea1b04546e242b0b0e23
xianjiezh <904292766@qq.com>
2017-12-15 18:38:48 +0800

    解决一个bug

--- pureJS/main.js
+++ pureJS/main.js
@@ -6,14 +6,16 @@ addButton.addEventListener('click', function () {
     let container = document.getElementById('id-div-container')
     let input = document.getElementById('id-input-todo')
     let toDo = input.value
-    container.innerHTML = template(toDo)
+    let t = template(toDo)
+    console.log(typeof(t))
+    container.insertAdjacentHTML('beforeend', t)
 })
 
 function template(todo) {
     let t = `
     <div class="todo-cell">
         <button class="todo-down">完成</button>
-        <button class="todo delete">删除</button>
+        <button class="todo-delete">删除</button>
         <span contenteditable="ture">${todo}</span>
     </div>
     `
