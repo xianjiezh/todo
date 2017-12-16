3e4bfeb5e5342c17d16d87753118732c7485f35c
xianjiezh <904292766@qq.com>
2017-12-16 23:48:39 +0800

    时间修正

--- pureJS/main.js
+++ pureJS/main.js
@@ -8,26 +8,38 @@ addButton.addEventListener('click', function () {
     let input = document.getElementById('id-input-todo')
     let todo = input.value
     // 添加到container中
-    insertTodo(todo,false)
+    insertTodo(todo,false,now())
     saveTodos()
 })
-function insertTodo(todo, done){
-    let t = template(todo,done)
+function insertTodo(todo, done,time){
+    let t = template(todo,done,time)
     container.insertAdjacentHTML('beforeend', t)
 }
-function template(todo,done) {
-    var status = ''
+function template(todo,done,time) {
+    let status = ''
     if(done){
-        var status = 'done'
+        let status = 'done'
     }
     let t = `
     <div class="todo-cell ${status}">
         <button class="todo-done">完成</button>
         <button class="todo-delete">删除</button>
         <span class="content">${todo}</span>
+        <span class="time"> ${time}</span>
     </div>
     `
     return t
+}
+function now(){
+
+    let d = new Date()
+    let year = d.getFullYear()
+    let month = d.getMonth() +1
+    let date = d.getDate()
+    let hour = d.getHours()
+    let min = d.getMinutes()
+    let second = d.getSeconds()
+    return `${year}/${month}/${date}/ ${hour}:${min}:${second}`
 }
 
 container.addEventListener('click', function (event) {
@@ -66,22 +78,27 @@ function load() {
 function saveTodos(){
     let content = document.getElementsByClassName('content')
     let todos = []
+    
     for (let i = 0; i < content.length; i++) {
         let c = content[i].textContent
         let done = content[i].parentElement.classList.contains('done')
+        let time = document.getElementsByClassName('time')[i]
         var todo = {
             content: c,
-            done:done
-        }
+            done:done,
+            // 得到页面中事件栏里面的文字
+            time:time.textContent                                    // 得到页面中事件栏里面的文字
+        } 
+        // 得到的哈希全部放到数组里面
         todos.push(todo)
     }
     save(todos)
-    console.log('localStorage' + ' ' + localStorage.todos)
 }
 function loadTodos(){
     let todos = load()
     for (let i = 0; i < todos.length; i++) { 
-        insertTodo(todos[i].content,todos[i].done)
+        // 拿到localStorage里面的数字，并得到每一个哈希
+        insertTodo(todos[i].content,todos[i].done,todos[i].time)
     }
     console.log('load'+todos)
 }
