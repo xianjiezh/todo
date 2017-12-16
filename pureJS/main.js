ac956ae7ca91293e8cae4448449ee4d31b5ebe5e
xianjiezh <904292766@qq.com>
2017-12-16 21:37:39 +0800

    add to localStorage

--- pureJS/main.js
+++ pureJS/main.js
@@ -7,18 +7,15 @@ addButton.addEventListener('click', function () {
     let input = document.getElementById('id-input-todo')
     let toDo = input.value
     let t = template(toDo)
-    if (toDo !== '') {
-        container.insertAdjacentHTML('beforeend', t)
-    } else {
-        alert('请输入待完成事件')
-    }
+    container.insertAdjacentHTML('beforeend', t)
+    saveTodos()
 })
 function template(todo) {
     let t = `
     <div class="todo-cell">
         <button class="todo-done">完成</button>
         <button class="todo-delete">删除</button>
-        <span>${todo}</span>
+        <span class="content">${todo}</span>
     </div>
     `
     return t
@@ -27,27 +24,42 @@ function template(todo) {
 container.addEventListener('click', function (event) {
 
     let cell = document.getElementsByClassName('todo-cell')
-    console.log(cell)
-    console.log(event.target)
     let target = event.target
     for (let i = 0; i < cell.length; i++) {
         let doneButton = document.getElementsByClassName('todo-done')[i]
         let deleteButton = document.getElementsByClassName('todo-delete')[i]
         if (target === doneButton) {
             toggerClass(target)
-        } else if(target === deleteButton){
+        } else if (target === deleteButton) {
             target.parentElement.remove()
+            saveTodos()
         }
-        
     }
-    
-    
+
 })
 
 function toggerClass(target) {
     if (target.parentElement.classList.contains('done')) {
-        target.classList.remove('done')
+        target.parentElement.classList.remove('done')
     } else {
         target.parentElement.classList.add('done')
     }
+}
+function save(todos) {
+    let s = JSON.stringify(todos)
+    localStorage.todos = s
+}
+function load() {
+    let l = localStorage.todos
+    return JSON.parse(l)
+}
+function saveTodos(){
+    let content = document.getElementsByClassName('content')
+    let todos = []
+    for (let i = 0; i < content.length; i++) {
+        let c = content[i].textContent
+        todos.push(c)
+    }
+    save(todos)
+    console.log('localStorage' + ' ' + localStorage.todos)
 }
